301fd3b0abf19b9a8daf03269001f41219714ec6
joachim <joachim@videoplaza.com>
2013-08-22 18:18:09 +0200

    test

--- web.js
+++ web.js
@@ -4,6 +4,15 @@ app.use(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello Jocke!');
+	var pg = require('pg');
+	var sql = request.sql;
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  		client.query(sql, function(err, result) {
+    			done();
+    			if(err) return console.error(err);
+    			console.log(result.rows);
+  		});
+	});
 });
 
 var port = process.env.PORT || 5000;
