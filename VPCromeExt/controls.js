489282ef028aa34f321ea7dd0d1f0d5cc1a3ce3f
Jesper Jalvemo <jesper.jalvemo@videoplaza.com>
2013-08-31 15:58:54 +0200

    click in drop down might work.

--- VPCromeExt/controls.js
+++ VPCromeExt/controls.js
@@ -1,6 +1,24 @@
+var serverUrl = "http://sleepy-wave-6767.herokuapp.com";
 $(function(){
 	get("whoami", function(me){
 		console.log("i am ", me);
     	$("#remote-code").html(me.id);
+
+        $("#logo").click(function(){
+            $.ajax({
+                url: serverUrl + "/next",
+                data: JSON.stringify({viewerId: me.id}),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                type : 'POST',
+                success: function(responseData){
+                    console.log("success", responseData);
+                },
+                failure: function(errMsg) {
+                    console.log("failure", errMsg);
+                }
+            });
+        });
     });
+
 });
